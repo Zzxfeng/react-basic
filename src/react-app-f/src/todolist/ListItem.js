6e5fcb23adf2e155bc34d513f9d4cc578705e33f
yangxiaoying <zhangxufeng@xingyuntech.com>
2020-06-11 17:00:56 +0800

    update

--- src/react-app-f/src/todolist/ListItem.js
+++ src/react-app-f/src/todolist/ListItem.js
@@ -4,7 +4,7 @@
  * @Autor: Alex
  * @Date: 2020-05-29 08:51:29
  * @LastEditors: Alex
- * @LastEditTime: 2020-05-29 14:28:40
+ * @LastEditTime: 2020-06-04 15:17:50
  */
 import React, { Component } from "react"
 import PropTypes from 'prop-types'
@@ -29,7 +29,7 @@ class ListItem extends Component {
 }
 ListItem.propTypes = {
     test:PropTypes.string.isRequired,
-    content:PropTypes.string.isRequired,
+    content:PropTypes.object.isRequired,
     index:PropTypes.number,
     handleItemDel:PropTypes.func
 }
--- src/react-app-f/src/todolist/TodoList.js
+++ src/react-app-f/src/todolist/TodoList.js
@@ -4,7 +4,7 @@
  * @Autor: Alex
  * @Date: 2020-03-10 09:26:52
  * @LastEditors: Alex
- * @LastEditTime: 2020-05-29 09:57:08
+ * @LastEditTime: 2020-06-04 15:50:11
  */
 
 import React, { Component, Fragment } from "react"
@@ -23,9 +23,17 @@ class TodoList extends Component {
 		return (
 			<Fragment>
 				<label htmlFor="input">请输入内容</label>
-				<input id="input" type="text" value={this.state.inputValueStr} placeholder="请输入todo" onChange={this.handleInputChange}></input>
+				<input 
+				id="input" 
+				type="text" 
+				value={this.state.inputValueStr} 
+				placeholder="请输入todo" 
+				onChange={this.handleInputChange}>
+				</input>
 				<button onClick={this.handleSubmit}>提交</button>
-				<ul className="list-item">
+				<ul 
+				className="list-item"
+				ref={(ul)=>{this.ul = ul}}>
 					{this.state.TodoList.map((item, index) => {
 						return (
 							<ListItem 
@@ -50,6 +58,9 @@ class TodoList extends Component {
 		this.setState({
 			TodoList: [...this.state.TodoList, this.state.inputValueStr],
 			inputValueStr: ""
+		},()=>{
+			console.log('当数据更新后执行...')
+			console.log(this.ul)
 		});
 	}
 	handleItemDel(index){
